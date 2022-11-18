c64838850b8609706b108df071d903adae499431
Aman18111625 <amangupta326519@gmail.com>
2022-11-18 19:22:57 +0530

    .env

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
 });
 
 let userLogged = false;
-const homeStartingContent =
-  "lorem espopahgsdjhfa hfaudsoihdsn sdaou;idnmdsansfodin sadhfdskhjhfds";
+const homeStartingContent = process.env.homeStartingContent
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
